1facf28d24bba5d67c2de9cabaea40ac34a1e658
Anik-Nath <anikdevnath03112000@gmail.com>
2021-10-20 15:48:58 +0600

    more

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -4,17 +4,20 @@ import { useParams } from "react-router";
 const Details = () => {
   const { serviceId } = useParams();
   const [ourServices, setOurServices] = useState([]);
+  const [singlee,setSinglee] = useState({});
 
     useEffect(() => {
     fetch("/services.json")
       .then((res) => res.json())
       .then((data) => setOurServices(data));
     }, []);
+    // console.log(ourServices)
 
-
-  const result = ourServices.find((e) => e.id === 2)
-  // console.log(result);
-
+    useEffect(()=>{
+      const gotYou  = ourServices.find(e => e.id === parseInt(serviceId))
+      // console.log(gotYou);
+      setSinglee(gotYou);
+    },[singlee])
 
 
   return (
